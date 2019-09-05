b4002b1cb503ec4992868eb317e48af0ad70b344
abechoi <abraham.choi@icloud.com>
2019-09-04 20:49:54 -0700

    Prompt and Confirm

---
+++ promptconfirm.js
@@ -0,0 +1,19 @@
+/*
+	PROMPT / CONFIRM
+
+	Examples:
+	let age = prompt('How old are you?', 100);
+	alert(`You are ${age} years old!`);
+
+	let ageReq = confirm("Are you atleast 18 years old?");
+	alert(ageReq); // true
+
+	Tasks:
+	1. A simple page
+*/
+
+'use strict';
+
+let name = prompt("What is your name?", "");
+alert(`Welcome, ${name}!`);
+
