3951d2d356d66b0e2a969981dd7fea596577fa9c
abechoi <abraham.choi@icloud.com>
2019-09-06 23:07:58 -0700

    Arrows

---
+++ arrows.js
@@ -0,0 +1,28 @@
+/*
+	ARROWS
+	Tasks:
+	1. Rewrite function with arrows:
+
+	function ask(question, yes, no) {
+  	if (confirm(question)) yes()
+ 	 else no();
+	}
+
+	ask(
+  	"Do you agree?",
+  	function() { alert("You agreed."); },
+  	function() { alert("You canceled the execution."); }
+	);
+*/
+
+let ask = (question, yes, no) => {
+  if (confirm(question)) yes()
+  else no();
+};
+
+
+ask(
+  "Do you agree?",
+  () => { alert("You agreed."); },
+  () => { alert("You canceled the execution."); }
+);
